2b357a0233acbc88167a09c348c57e8d3a3e0cc7
michelleling02 <93761096+michelleling02@users.noreply.github.com>
2025-09-25 12:47:48 +0800

    Updated analytics.tsx

--- app/(owner)/edit-profile.tsx
+++ app/(owner)/edit-profile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
-import { User, Edit } from 'lucide-react-native';
+import { User, CreditCard as Edit } from 'lucide-react-native';
 
 export default function EditProfileScreen() {
   return (
